d7c48942ce0dbe88b311fb9c20127cfc3b0e42a1
Sid <siddusiddharth789@gmail.com>
2025-08-22 22:48:40 +0530

    Revert "New commit"
    This reverts commit 7fa33941dbfa317c2597126cde7e75c2f5f5fe6d.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! My Name is Sid");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
