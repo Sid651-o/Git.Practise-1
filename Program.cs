defdf561a3dda8899d392b130bd0eb85dde67639
Sid <siddusiddharth789@gmail.com>
2025-08-22 15:17:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Git.Practise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
