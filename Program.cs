75ad8469d4d01c551a8ef8ccea6f08f974ee696a
Sid <siddusiddharth789@gmail.com>
2025-08-22 22:34:31 +0530

    Revert "Main commit"
    This reverts commit 2c025191525fdc38444b4054e16c8202fdf265c5.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! Wassuppp!!!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
