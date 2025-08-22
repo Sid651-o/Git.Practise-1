69f4762c91bf9f32a332e6055429fb08ad1201d7
Sid <siddusiddharth789@gmail.com>
2025-08-22 22:57:06 +0530

    #New Method Commit

---
+++ NewMethod1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Practise_1
+{
+    internal class NewMethod1
+    {
+        static void Display()
+        {
+            Console.WriteLine("My Method");
+        }
+    }
+}
